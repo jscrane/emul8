dc9cc870c13d94d2bc0dd15ccd9da8095b2ffd0a
steve <jscrane@gmail.com>
2023-09-06 09:55:36 +0100

    add -p flag

--- util/makerom.cc
+++ util/makerom.cc
@@ -2,24 +2,30 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <getopt.h>
 
 int main(int argc, char *argv[])
 {
-	int bin = 0, a = 1;
+	bool bin = false, progmem = false;
+	int opt;
 	
-	if (argc != 3 && argc != 4) {
-		fprintf(stderr, "Usage: %s: [-b] image-file array-name\n", argv[0]);
-		return -1;
-	}
-
-	if (strcmp(argv[a], "-b") == 0) {
-		bin = 1;
-		a++;
-	}
-	char *file = argv[a++], *name = argv[a];
+	while ((opt = getopt(argc, argv, "bp")) != -1)
+		switch (opt) {
+		case 'b':
+			bin = true;
+			break;
+		case 'p':
+			progmem = true;
+			break;
+		default:
+			fprintf(stderr, "Usage: %s: [-b] [-p] image-file array-name\n", argv[0]);
+			return -1;
+		}
+	int ind = optind;
+	char *file = argv[ind++], *name = argv[ind];
 	FILE *f = fopen(file, "r");
 
-	printf("static const unsigned char %s[] = {", name);
+	printf("static const uint8_t %s[] %s= {", name, progmem? "PROGMEM ": "");
 
 	for (int i = 0; ; i++) {
 		unsigned c;
